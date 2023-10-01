0919a5f09c97cc584f36b98657ad1511a5760ffe
Alexander <bmgula55@mail.ru>
2023-10-02 01:19:20 +0300

    add final result ))

--- main.py
+++ main.py
@@ -1,88 +1,99 @@
-import time
-
 from connector import Connector, PathNotFoundError
 from api import HH, Superjob, Vacancy, HHVacancy
 from utils import get_hh_vacancies_list, get_top, sorting, get_sj_vacancies_list, get_currency
 
 
 def main():
-    # keyword = input("Введите ключевое слово по которому будет осуществляться поиск вакансий:\n").lower().strip()
-    keyword = "python"
-    hh_engine = HH(keyword)
-    sj_engine = Superjob(keyword)
+    count = 0
+    while count == 0:
+        menu = input(
+            "Выберите действие:\n1.Загрузить актуальные данные с 'HH','SJ' в JSON файл\n2.Поиск вакансий (JSON файла)\n3.Завершить работу программы\n")
 
-    hh_path_json = "hh_vacancies.json"
-    sj_path_json = "sj_vacancies.json"
+        hh_path_json = "hh_vacancies.json"
+        sj_path_json = "sj_vacancies.json"
+        hh_connector = Connector(hh_path_json)
+        sj_connector = Connector(sj_path_json)
+        hh_vacancies_list = get_hh_vacancies_list(hh_connector)
+        sj_vacancies_list = get_sj_vacancies_list(sj_connector)
 
-    hh_connector = Connector(hh_path_json)
-    sj_connector = Connector(sj_path_json)
+        if menu == "1":
+            keyword = input("Введите ключевое слово по которому будет осуществляться поиск вакансий:\n").lower().strip()
 
-    page = 0
-    hh_pages = 1
-    hh_close = False
-    more = True
+            hh_engine = HH(keyword)
+            sj_engine = Superjob(keyword)
 
-    all_vacancy = []
-    hh_all_vacancy = [0]
-    sj_all_vacancy = [0]
+            page = 0
+            hh_pages = 1
+            hh_close = False
+            more = True
 
-    while not hh_close or more:
-        if page < hh_pages:
-            hh_engine.params["page"] = page
-            page += 1
-            hh_vacancies = hh_engine.get_request()
-            hh_pages = hh_vacancies["pages"]
-            hh_items = hh_vacancies["items"]
-            hh_all_vacancy.append(len(hh_items))
-            all_vacancy.append(hh_items[0])
-            print(f"HH:{page}/{hh_pages}")
-            hh_connector.insert(hh_items)
-        else:
-            hh_close = True
+            all_vacancy = []
+            hh_all_vacancy = [0]
+            sj_all_vacancy = [0]
 
-        if more:
-            sj_engine.params["page"] = sj_engine.params["page"] + 1
-            sj_vacancies = sj_engine.get_request().json()
-            sj_items = sj_vacancies["objects"]
-            more = sj_vacancies["more"]
-            sj_all_vacancy.append(len(sj_items))
-            print(f'SJ:{sj_engine.params["page"]}/{len(sj_items)}')
-            sj_connector.insert(sj_items)
+            while not hh_close or more:
+                try:
+                    if page < hh_pages:
+                        hh_engine.params["page"] = page
+                        page += 1
+                        hh_vacancies = hh_engine.get_request()
+                        hh_pages = hh_vacancies["pages"]
+                        hh_items = hh_vacancies["items"]
+                        hh_all_vacancy.append(len(hh_items))
+                        all_vacancy.append(hh_items[0])
+                        print(f"HH:{page}/{hh_pages}")
+                        hh_connector.insert(hh_items)
+                    else:
+                        hh_close = True
 
-    print(f"HH: Найдено {sum(hh_all_vacancy)} вакансий по ключевому слову {keyword}")
-    print(f"SJ: Найдено {sum(sj_all_vacancy)} вакансий по ключевому слову {keyword}")
+                    if more:
+                        sj_engine.params["page"] = sj_engine.params["page"] + 1
+                        sj_vacancies = sj_engine.get_request().json()
+                        sj_items = sj_vacancies["objects"]
+                        more = sj_vacancies["more"]
+                        sj_all_vacancy.append(len(sj_items))
+                        print(f'SJ:{sj_engine.params["page"]}/{len(sj_items)}')
+                        sj_connector.insert(sj_items)
+                except Exception as e:
+                    pass
 
-    res_input = input("Введите команду (sort или top):\n").lower().strip()
-    hh_vacancies_list = get_hh_vacancies_list(hh_connector)
-    sj_vacancies_list = get_sj_vacancies_list(sj_connector)
+            print(f"HH: Найдено {sum(hh_all_vacancy)} вакансий по ключевому слову {keyword}")
+            print(f"SJ: Найдено {sum(sj_all_vacancy)} вакансий по ключевому слову {keyword}")
 
-    if res_input == "top":
-        set_currency = get_currency(hh_vacancies_list)
-        print("Выберите валюту:", *set_currency, sep="\n")
-        currency = input()
-        if currency in set_currency:
-            pass
+        if menu == "2":
+            if hh_vacancies_list or sj_vacancies_list:
 
-        top_n = input("Введите количество вакансий для вывода в топ N: ")
-        if top_n.isdigit():
-            print(*get_top(hh_vacancies_list, int(top_n)), sep="\n")
-        else:
-            print("Не верно введены данные: введите число")
-    if res_input == "sort":
-        print(*sorting(hh_vacancies_list), sep="\n")
+                res_input = input("Введите команду (sort или top):\n").lower().strip()
 
+                if res_input == "top":
+                    all_сurrency = set(i.сurrency for i in hh_vacancies_list + sj_vacancies_list if i.сurrency != "")
 
-#
-# try:
-#         сurrency = int(input(f"Выберите цифру валюты:\n{a}"))
-#         if сurrency in a:
-#             result = a[сurrency]
-#         else:
-#             print("Не верно указана цифра")
-#
-# except PathNotFoundError:
-#     print(f"Дириктория {hh_path_json} не найдена.")
-#     return
+                    print("Выберите валюту:", sep="\n")
+                    [print(i) for i in all_сurrency]
+                    currency = input().upper().strip()
+                    if currency in all_сurrency:
+                        res = get_currency(hh_vacancies_list + sj_vacancies_list, currency)
+                    else:
+                        print("Не корректные данные, повторите попытку")
+                        continue
+
+                    top_n = input("Введите количество вакансий для вывода в топ N: ")
+                    if top_n.isdigit():
+                        print(*get_top(res, int(top_n)), sep="\n")
+                    else:
+                        print("Не корректные данные, повторите попытку")
+                        continue
+
+                elif res_input == "sort":
+                    print(*sorting(hh_vacancies_list + sj_vacancies_list), sep="\n")
+                else:
+                    print("Не корректные данные, повторите попытку")
+                    continue
+            else:
+                print("Файл пуст загрузите данные!")
+
+        elif menu == "3":
+            exit()
 
 
 main()
--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ from api import Vacancy, HHVacancy, SJVacancy
 
 def sorting(vacancies: list[Vacancy]):
     """ Должна сортировать любой список вакансий по ежемесячной оплате"""
-    return sorted(vacancies.сurrency)
+    return sorted(vacancies)
 
 
 def get_top(vacancies: list[Vacancy], top_count: int):
@@ -11,15 +11,13 @@ def get_top(vacancies: list[Vacancy], top_count: int):
     return list(sorted(vacancies, reverse=True)[:top_count])
 
 
-def get_currency(vacancies: list[Vacancy]):
-    """ Должен возвращать список картежей пронумерованной валюты """
-    count = 1
-    res = []
-    list_vacancies = [i.сurrency for i in vacancies]
-    for i in set(list_vacancies):
-        res.append({count: i})
-        count += 1
-    return res
+def get_currency(vacancies: list[Vacancy], сurrency):
+    """ Должен возвращать список вакансий отсортированной по параметру валюты - сurrency"""
+    сurrency_list = []
+    for i in vacancies:
+        if i.сurrency == сurrency:
+            сurrency_list.append(i)
+    return сurrency_list
 
 
 def get_hh_vacancies_list(connector):
@@ -35,12 +33,13 @@ def get_hh_vacancies_list(connector):
 
 
 def get_sj_vacancies_list(connector):
+
     vacancies = [
         SJVacancy(
             name=vacancy["profession"],
             url=vacancy["link"],
             description=vacancy["candidat"],
             salary=vacancy["payment_from"],
-            сurrency=vacancy["currency"])
+            сurrency=str(vacancy["currency"]).upper() if vacancy["currency"] not in ("RUB", "rub") else 'RUR')
         for vacancy in connector.select({})]
     return vacancies
